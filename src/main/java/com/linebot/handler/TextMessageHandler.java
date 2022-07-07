c00e284410ba9386d9b9793b83f39f8a4459de5a
Ansonchan9 <108714794+Ansonchan9@users.noreply.github.com>
2022-07-07 12:17:27 +0800

    Update TextMessageHandler.java

--- src/main/java/com/linebot/handler/TextMessageHandler.java
+++ src/main/java/com/linebot/handler/TextMessageHandler.java
@@ -166,7 +166,7 @@ public class TextMessageHandler {
                 //            final List<ImagemapAction> actions) {
                 this.reply(replyToken, ImagemapMessage
                         .builder()
-                        .baseUrl(createUri("https://www.shutterstock.com/zh-Hant/category/illustrations-clip-art"))
+                        .baseUrl(createUri("/static/rich"))
                         .altText("This is alt text")
                         .baseSize(new ImagemapBaseSize(1040, 1040))
                         .actions(Arrays.asList(
